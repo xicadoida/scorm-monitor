eaa5d7c86284b894a471466ae5dca35a9c293feb
xicadoida <moises.mgas@gmail.com>
2026-06-16 14:27:23 -0300

    configuração inicial para servidor render

--- backend/main.py
+++ backend/main.py
@@ -25,7 +25,7 @@ app.include_router(classes_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://lms.iaclube.help/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
